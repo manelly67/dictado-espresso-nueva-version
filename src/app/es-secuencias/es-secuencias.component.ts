c2b26e5c5f67b00483381dc576a448fac9136ffc
manelly67 <manellylopezqqq@gmail.com>
2023-11-04 15:41:30 -0500

    version23 nov2023

--- src/app/es-secuencias/es-secuencias.component.ts
+++ src/app/es-secuencias/es-secuencias.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { MensajeService } from '../mensaje.service';
+
 @Component({
   selector: 'app-es-secuencias',
   templateUrl: './es-secuencias.component.html',
@@ -10,9 +12,19 @@ import { Location } from '@angular/common';
 export class EsSecuenciasComponent implements OnInit {
 
 constructor(private route: ActivatedRoute,
-				private location: Location ) { }
+				private location: Location,
+					public mensajeService: MensajeService   // solamente define el título de la página de ejercicios correspondiente
+				) { }
 				
 idioma:string ='';
+
+	// inicio definir título superior de la categoría de la página de ejercicios
+	categoria = this.mensajeService.categoria;
+	categorias = this.mensajeService.captarCategorias();
+	inicialnivel:number= 6;
+	finalnivel:number= 7;
+	espacio:string=' - ';
+	// final definir título superior de la categoría de la página de ejercicios
 			
 	ngOnInit(){
 	this.idioma='spanish';
--- src/app/secuencias/secuencias.component.ts
+++ src/app/secuencias/secuencias.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { MensajeService } from '../mensaje.service';
+
 
 @Component({
   selector: 'app-secuencias',
@@ -11,9 +13,19 @@ import { Location } from '@angular/common';
 export class SecuenciasComponent implements OnInit {
 
 constructor(private route: ActivatedRoute,
-				private location: Location ) { }
+				private location: Location,
+					public mensajeService: MensajeService   // solamente define el título de la página de ejercicios correspondiente
+					) { }
 
 idioma:string ='';
+
+	// inicio definir título superior de la categoría de la página de ejercicios
+	categoria = this.mensajeService.categoria;
+	categorias = this.mensajeService.captarCategorias();
+	inicialnivel:number=16;
+	finalnivel:number=17;
+	espacio:string=' - ';
+	// final definir título superior de la categoría de la página de ejercicios
 			
 	ngOnInit(){
 	this.idioma='portuguese';
